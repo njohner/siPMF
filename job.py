26f27e941097b20826a9ac716e82c7787ec445fd
Niklaus Johner <niklaus.johner@unibas.ch>
2016-01-18 11:52:11 +0100

    Added documentation for Job class.

--- job.py
+++ job.py
@@ -1,10 +1,26 @@
+"""
+.. codeauthor:: Niklaus Johner <niklaus.johner@a3.epfl.ch>
+
+This file contains the :class:`Job` object which represents a job on a cluster.
+"""
 import os
 
 class Job():
+  """
+  This class is the bottom of the hierarchical structure used in SiPMF. There is one :class:`Job`
+  object for every :class:`Phase` of every :class:`Window`. This object is used to submit a job
+  to the cluster and check its status
+  """
   def __repr__(self):
     return "Job({0})".format(self.phase)
 
   def __init__(self,phase):
+    """
+    Creates a new :class:`Job` instance.
+
+    :param phase: The corresponding simulation phase.
+    :type phase:  :class:`Phase`
+    """
     self.phase=phase
     self.status="To submit"
     self.GenerateInputFile()
@@ -12,6 +28,11 @@ class Job():
     elif self.phase.type=="run":self.path_to_job_file=os.path.join(self.phase.window.system.basedir,self.phase.window.system.run_job_fname)
   
   def GenerateInputFile(self):
+    """
+    Generate the MD input file. This function reads the template input file (either the *initialization input file*
+    or the *run input file*) and replaces several fields by their corresponding values, such as the values
+    of the collective variables and spring constants.
+    """
     if self.phase.type=="initialization":
       f=open(self.phase.window.system.GetPathToInitInputFile(),"r")
       replace=self.ReplaceInInitInput
@@ -59,11 +80,23 @@ class Job():
     return f_data_new
 
   def Submit(self,environment):
+    """
+    Submit the job to the cluster
+
+    :param environment: The environment used to submit the job
+    :type environment: :class:`Environment`
+    """
     self.jid=environment.qsub(self.phase.outdir,self.path_to_job_file)
     self.status="submitted"
     self.phase.window.system.unfinished_jobs.append(self)
 
   def UpdateStatus(self,environment):
+    """
+    Check whether the job is still in the queue
+
+    :param environment: The environment used to check the job status
+    :type environment: :class:`Environment`
+    """
     if self.status!="finished":
       self.status=environment.qstat(self.jid)
       if self.status=="finished" and self.phase.type=="run":
