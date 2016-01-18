ad332fdb8d86735aa01cac5df1b26f7345d124a3
Niklaus Johner <niklaus.johner@unibas.ch>
2016-01-18 12:26:49 +0100

    Added __str__ method to Phase to avoid the recursive output obtained using the __repr__ (due to parent phases which also have parents and so on).

--- phase.py
+++ phase.py
@@ -5,6 +5,13 @@ class Phase():
   def __repr__(self):
     return "Phase({0},{1},{2},{3})".format(self.window,self.name,self.type,self.parent_phase)
   
+  def __str__(self):
+    cvs=" , ".join([cv.name+"="+str(cv_val) for cv,cv_val in zip(self.window.system.cv_list,self.window.cv_values)])
+    if self.parent_phase:
+      cvs2=" , ".join([cv.name+"="+str(cv_val) for cv,cv_val in zip(self.parent_phase.window.system.cv_list,self.parent_phase.window.cv_values)])
+      return "Phase {0}: This is a {1} phase with {2} and initialized form {3}".format(self.name,self.type,cvs,cvs2)
+    else:return "Phase {0}: This is a {1} phase with {2}".format(self.name,self.type,cvs)
+  
   def __init__(self,window,phase_name,phase_type,parent_phase=None):
     self.window=window
     self.name=phase_name
@@ -22,7 +29,7 @@ class Phase():
     #self.outname=self.name
 
   def Initialize(self):
-    print self,self.outdir
+    print "New",self
     r=subprocess.call(["mkdir",self.outdir])
     if r!=0:raise IOError("Problem creating an output directory.")
     self.job=Job(self)
