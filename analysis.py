39898faf9524a7ffaf9022e43043e5038f5abdc0
Niklaus Johner <niklaus.johner@unibas.ch>
2016-07-25 09:10:23 +0200

    Added function to analyze and plot variables for every window of the PMF

--- analysis.py
+++ analysis.py
@@ -6,6 +6,7 @@ This module contains the analysis function for trajectories genereated with a si
 import os,subprocess,logging
 import numpy as npy
 import matplotlib.pyplot as plt
+from matplotlib.mlab import griddata
 from ost import *
 
 __all__=('BuildTrajectory')
@@ -32,4 +33,49 @@ def BuildTrajectory(system,windows,eh,traj_filename,stride=1):
     vl=t.GetFramePositions(t.GetFrameCount()-1)
     t_out.AddFrame(vl)
   return t_out
+
+
+def AnalyzeWindows(system,eh,analysis_function,function_arguments,traj_filename,first_phase=0,first_frame=0,stride=1,windows=None):
+  if not windows:windows=system.windows
+  res_dict={}
+  for w in windows:
+    res=[]
+    for p in w.phases[first_phase:]:
+      t=io.LoadCHARMMTraj(eh,os.path.join(p.outdir,traj_filename),stride=stride)
+      res.extend(analysis_function(t,*function_arguments)[first_frame:])
+    res_dict[w]=res
+  return res_dict
+
+def PlotWindowAverage(system,res_dict,title=None,outname=None):
+  dimensionality=system.dimensionality
+  if dimensionality>2:
+    print "can only plot averages for systems with dimensionality 1 or 2"
+    return
+  data=[]
+  for w in res_dict:
+    m=npy.average(res_dict[w])
+    if npy.isnan(m):continue
+    res=list(w.cv_values)
+    res.append(m)
+    data.append(res)
+  data=npy.array(data)
+  f=plt.figure()
+  if dimensionality==1:
+    plt.plot(data[:,0],data[:,1],'o')
+    plt.xlabel("{0} [{1}]".format(system.cv_list[0].name,system.cv_list[0].units))
+    plt.ylabel("Average")
+  elif dimensionality==2:
+    xl=[]
+    for cv in system.cv_list:
+      xl.append(npy.linspace(cv.min_value,cv.max_value,cv.num_bins))
+    xi,yi=npy.meshgrid(*xl)
+    zi=griddata(data[:,0],data[:,1],data[:,2], xi, yi)
+    plt.contourf(xi, yi, zi)
+    plt.colorbar()
+    plt.xlabel("{0} [{1}]".format(system.cv_list[0].name,system.cv_list[0].units))
+    plt.ylabel("{0} [{1}]".format(system.cv_list[1].name,system.cv_list[1].units))
+  if title:plt.title(title)
+  if outname:plt.savefig(outname)
+  return f
+
 
