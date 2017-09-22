7b3a4951dcf89bf244a463bc96e1bc69d22bff52
Niklaus Johner <niklaus.johner@unibas.ch>
2017-09-22 12:21:50 +0200

    Add package initialization

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from sipmf import SiPMF
+from environment import Environment
+from colvar import CollectiveVariable
+from system import System
+__all__ = ["Environment", "CollectiveVariable", "SiPMF","System"]
